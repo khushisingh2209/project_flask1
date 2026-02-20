9744afd89c69bb55f31c0d771bf3a14cc81d482a
khushisingh2209 <khushiasingh987@gmail.com>
2026-02-20 12:28:37 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# databaseconnection.py
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Khushi@123",
+        database="pypro"
+    )
